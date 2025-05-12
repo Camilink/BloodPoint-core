bcbeab54670df3a4d4d60c64bd10c7eb0c230170
Camila <camila.jopia@ltec.cl>
2025-05-12 00:24:50 -0400

    superset config

--- superset_config.py
+++ superset_config.py
@@ -5,7 +5,7 @@ ROW_LIMIT = 5000
 SECRET_KEY = 'Okiqzq/LVgWIDvxZ5nCU4bzNxA4Hyi37VD0dIQUeeB8qjaTv39XfJw1v'
 SQLALCHEMY_DATABASE_URI = 'postgres://u5mh9fi08iuct0:pb0d6bd9f0e847a780e5403a376a825847485c97a50a2dd1459a86dc144440ced@ca932070ke6bv1.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d50aqolcf988ab'
 SUPERSET_WEBSERVER_PORT = 8088
-SUPERSET_CONFIG_PATH = "/app/superset_config.py"
+SUPERSET_CONFIG_PATH = "superset_config.py"
 
 
 
