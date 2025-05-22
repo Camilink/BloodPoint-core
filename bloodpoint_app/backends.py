59ddff73dacc97cbff2fcf5bb10f880232e5a265
Camila <camila.jopia@ltec.cl>
2025-05-22 01:14:48 -0400

    fix backend login

--- bloodpoint_app/backends.py
+++ bloodpoint_app/backends.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.backends import BaseBackend
 from bloodpoint_app.models import CustomUser
 
+
 class RutAuthBackend(BaseBackend):
     def authenticate(self, request, rut=None, password=None, **kwargs):
         try:
@@ -15,3 +16,21 @@ class RutAuthBackend(BaseBackend):
             return CustomUser.objects.get(pk=user_id)
         except CustomUser.DoesNotExist:
             return None
+# bloodpoint_app/backends.py
+
+class EmailOrRutBackend(ModelBackend):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        user = None
+        if username is None:
+            username = kwargs.get('email') or kwargs.get('rut')
+        try:
+            user = CustomUser.objects.get(email=username)
+        except CustomUser.DoesNotExist:
+            try:
+                user = CustomUser.objects.get(rut=username)
+            except CustomUser.DoesNotExist:
+                return None
+        
+        if user.check_password(password) and self.user_can_authenticate(user):
+            return user
+        return None
