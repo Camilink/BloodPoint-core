e4b81e6f88f5c0e548ceed3dec9e6b60af47ed6f
Camila <camila.jopia@ltec.cl>
2025-05-31 22:15:15 -0400

    superset utils

--- bloodpoint_app/superset_utils.py
+++ bloodpoint_app/superset_utils.py
@@ -4,13 +4,15 @@ import time
 def generate_superset_embed_token(dashboard_uuid):
     now = int(time.time())
     payload = {
-        "resource": {"dashboard": dashboard_uuid},
+        "resources": [  # ← aquí el cambio
+            {"type": "dashboard", "id": dashboard_uuid}
+        ],
         "params": {},
         "exp": now + 3600,
         "iat": now,
         "aud": "superset_embedded",
         "iss": "bloodpoint-core-qa",
-        "sub": "admin_user",
+        "sub": "admin_user",  # o cualquier string de usuario ficticio
     }
 
     secret = "django-insecure-08&ko%+7k8l=v1-@1y@1g-(7ht_uc816k#_&nt@uncpc^ki$jp"
