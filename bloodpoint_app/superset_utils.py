3adba68ab4b4cc7afa10419203aceb0cc141bcc9
Camila <camila.jopia@ltec.cl>
2025-05-31 22:30:47 -0400

    sub user edit

--- bloodpoint_app/superset_utils.py
+++ bloodpoint_app/superset_utils.py
@@ -12,7 +12,7 @@ def generate_superset_embed_token(dashboard_uuid):
         "iat": now,
         "aud": "superset_embedded",
         "iss": "bloodpoint-core-qa",
-        "sub": "admin_user",  # o cualquier string de usuario ficticio
+        "sub": "guest",  # o cualquier string de usuario ficticio
     }
 
     secret = "django-insecure-08&ko%+7k8l=v1-@1y@1g-(7ht_uc816k#_&nt@uncpc^ki$jp"
