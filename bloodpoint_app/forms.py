e92c5545cba9ab8bd50f5fa071ccdc380bd47c05
Camila <camilaajojeda@gmail.com>
2025-06-14 17:35:19 -0400

    fix representante form

--- bloodpoint_app/forms.py
+++ bloodpoint_app/forms.py
@@ -12,15 +12,28 @@ class AdminBPForm(forms.ModelForm):
         }
 
 class RepresentanteOrgForm(forms.ModelForm):
+from django import forms
+from .models import representante_org
+
+class RepresentanteOrgForm(forms.ModelForm):
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={'class': 'form-control', 'disabled': 'disabled'}),
+        required=False  # Since it's not a model field, avoid validation errors
+    )
+
     class Meta:
         model = representante_org
-        fields = ['nombre', 'apellido','email', 'rut_representante', 'rol', 'credencial']
+        fields = ['nombre', 'apellido', 'rut_representante', 'rol', 'credencial']
         widgets = {
             'nombre': forms.TextInput(attrs={'class': 'form-control'}),
             'apellido': forms.TextInput(attrs={'class': 'form-control'}),
-            'email': forms.EmailInput(attrs={'class': 'form-control', 'disabled': 'disabled'}),
             'rut_representante': forms.TextInput(attrs={'class': 'form-control'}),
             'user': forms.Select(attrs={'class': 'form-control'}),
             'rol': forms.TextInput(attrs={'class': 'form-control'}),
             'credencial': forms.FileInput(attrs={'class': 'form-control'}),
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.instance and hasattr(self.instance, 'user'):
+            self.fields['email'].initial = self.instance.user.email
