0715ad7cafe229fbfba9ed9ff9b321ddfffb1c2a
Camila <camila.jopia@ltec.cl>
2025-06-17 01:57:18 -0400

    forms.py cloudinary

--- bloodpoint_app/forms.py
+++ bloodpoint_app/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import representante_org, adminbp
+from cloudinary.forms import CloudinaryFileField
 
 class AdminBPForm(forms.ModelForm):
     class Meta:
@@ -14,19 +15,20 @@ class AdminBPForm(forms.ModelForm):
 class RepresentanteOrgForm(forms.ModelForm):
     email = forms.EmailField(
         widget=forms.EmailInput(attrs={'class': 'form-control', 'disabled': 'disabled'}),
-        required=False  # Since it's not a model field, avoid validation errors
+        required=False 
     )
 
     class Meta:
         model = representante_org
-        fields = ['nombre', 'apellido', 'rut_representante', 'rol', 'credencial']
+        image = CloudinaryFileField(required=False)
+        fields = ['nombre', 'apellido', 'rut_representante', 'rol', 'image']
         widgets = {
             'nombre': forms.TextInput(attrs={'class': 'form-control'}),
             'apellido': forms.TextInput(attrs={'class': 'form-control'}),
             'rut_representante': forms.TextInput(attrs={'class': 'form-control'}),
             'user': forms.Select(attrs={'class': 'form-control'}),
             'rol': forms.TextInput(attrs={'class': 'form-control'}),
-            'credencial': forms.FileInput(attrs={'class': 'form-control'}),
+            'image': forms.FileInput(attrs={'class': 'form-control'}),
         }
 
     def __init__(self, *args, **kwargs):
